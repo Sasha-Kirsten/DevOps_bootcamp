43554457d8baf668c70cfb500bdcebb5f84db700
[s2545171] <aleksanderkirsten@googlemail.com>
2026-07-11 11:33:16 +0100

    Building the skeleton function for the groovy script and the Jenkinsfile

---
+++ Module_8_Build_Automation_CI_CD/Dynamically_Increment_Application_version_Jenkins_Pipeline/CI_step.groovy
@@ -0,0 +1,24 @@
+def Increment_Version(String currentVersion){
+
+}
+
+// def Build_Java_Application(String mavenApplication){
+
+// }
+
+def Maven_clean_and_build(String mavenApplication){
+
+}
+
+def Build_Docker_Image(String dockerImageName, String dockerTag){
+
+}
+
+def Push_Docker_Image(String dockerImageName, String dockerTag, String dockerUsername, String dockerPassword){
+
+}
+
+def Commit_Version_Update(String gitUsername, String gitEmail, String gitBranch, String gitCommitMessage){
+
+}
+
