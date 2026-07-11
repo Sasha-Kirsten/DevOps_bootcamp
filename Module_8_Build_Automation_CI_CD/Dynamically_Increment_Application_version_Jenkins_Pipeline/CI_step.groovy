99b10ac8e01208ea3af9b19959a7127ee6a90ec1
[s2545171] <aleksanderkirsten@googlemail.com>
2026-07-11 22:04:29 +0100

    Changes the function for the CI_pipleline.groovy

--- Module_8_Build_Automation_CI_CD/Dynamically_Increment_Application_version_Jenkins_Pipeline/CI_step.groovy
+++ Module_8_Build_Automation_CI_CD/Dynamically_Increment_Application_version_Jenkins_Pipeline/CI_step.groovy
@@ -1,33 +1,26 @@
 def Increment_Version(String currentVersion){
-    versionParts = currentVersion.tokenize('.')
-    patchVersion = versionParts[2].toInteger() 
-    minorVersion = versionParts[1].toInteger()
-    majorVersion = versionParts[0].toInteger()
-    if(patch_version == 9 and minorVersion == 9){
+    def versionParts = currentVersion.tokenize('.')
+    def majorVersion = versionParts[0].toInteger()
+    def minorVersion = versionParts[1].toInteger()
+    def patchVersion = versionParts[2].toInteger() 
+    
+    // Fixed Groovy syntax: '&&' instead of 'and', 'else if' instead of 'elif'
+    if(patchVersion == 9 && minorVersion == 9){
         patchVersion += 1
-    } elif(patchVersion == 9 and minorVersion > 9){
+    } else if(patchVersion == 9 && minorVersion > 9){
         patchVersion = 0
         minorVersion = minorVersion + 1
         versionParts[1] = minorVersion.toString()
-
-    }
-    else {
-        patchVersion = 0
-        minorVersion = versionParts[1].toInteger() + 1
-        versionParts[1] = minorVersion.toString()
+    } else {
+        patchVersion += 1
     }
 
     return "${majorVersion}.${minorVersion}.${patchVersion}"
 }
 
-// def Build_Java_Application(String mavenApplication){
-
-// }
-
 def Maven_clean_and_build(String mavenApplication){
     sh "mvn clean package -f ${mavenApplication}/pom.xml"
     sh "mvn clean install -f ${mavenApplication}/pom.xml"
-
 }
 
 def Build_Docker_Image(String dockerImageName, String dockerTag){
@@ -44,6 +37,5 @@ def Commit_Version_Update(String gitUsername, String gitEmail, String gitBranch,
     sh "git config user.email ${gitEmail}"
     sh "git add ."
     sh "git commit -m '${gitCommitMessage}'"
-    sh "git push origin ${gitBranch}"   
+    sh "git push origin ${gitBranch}"
 }
-
